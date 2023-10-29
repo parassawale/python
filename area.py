71dd8157d4d6feaef8af007b14f3d5340aa047be
Paras Sawale <147709064+parassawale@users.noreply.github.com>
2023-10-29 18:31:17 +0530

    area.py
    area of the 
    1)triangle
    2)circle
    3)rectangle

---
+++ area.py
@@ -0,0 +1,9 @@
+r=float(input("enter the radius of the circle"))
+area_c=3.14*r*r
+print("THE AREA OF THE CIRCLE IS ",area_c)
+a=int(input("ENTER THE LENGTH OF SIDE OF base"))
+b=int(input("ENTER THE LENGTH OF SIDE OF height"))
+area_r=a*b
+area_t=a*b*0.5
+print("THE AREA OF TRIANGLE IS ",area_t)
+print("THE AREA OF THE RECTANGLE IS ",area_r)
