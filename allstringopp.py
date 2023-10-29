2cb3593c272920f6950c39032e6a83b5e158096e
Paras Sawale <147709064+parassawale@users.noreply.github.com>
2023-10-29 18:29:51 +0530

    allstringopp.py
    it have all string operation in python 
    1)concat
    2)if else
    3)palidrome
    4)etc

---
+++ allstringopp.py
@@ -0,0 +1,21 @@
+a="paras"
+b="dasara"
+c=a+b
+q=set(a)&set(b)
+print(q)
+if a in c:
+    print("yes")
+else:
+    print("no")
+e="nayan"
+rev=e[::-1]
+if(e==rev):
+    print("it is paralidrome")
+else:
+    print("it is not a palaidrome")
+z=input("enter the string ")
+y=0
+for i in z:
+    if(i.islower()):
+     y=y+1
+print(y)                    
